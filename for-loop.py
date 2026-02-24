542a4476ce9342450160f7e2906d94b22b975f3b
srikanth-suthari <suthari.srikanth@outlook.com>
2026-02-24 22:44:35 +0530

    python

--- for-loop.py
+++ for-loop.py
@@ -3,6 +3,6 @@ for i in range(1,6):
 
 # Nested for loop with i and j
 for i in range(0,2):
-    for j in range(0,5):
-        print(j)
-    print(i)
+    for j in range(1,2):
+        print(i)
+    print(j)
