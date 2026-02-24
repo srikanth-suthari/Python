8c3ce6d71a6bc251bd6c1bf49b4fa87d4690b871
srikanth-suthari <suthari.srikanth@outlook.com>
2026-02-24 22:35:04 +0530

    python

---
+++ variables.py
@@ -0,0 +1,33 @@
+fname = "Srikanth"
+lname = "Chary"
+
+# concatinating the two names into a single name
+
+full_name = fname + ' ' + lname
+
+print(full_name)
+
+int = 10
+print(int)
+print()
+
+float = 1.23
+print(float)
+print()
+
+str = "Srikanth"
+print(str)
+print()
+
+list = [1,2,'a','b',1.23]   # or array
+print(list)
+print()
+
+set = {1,4,2,9,5,}
+print(set)
+print()
+
+dict = {'name':'Srikanth Chary'}
+print(dict)
+print(dict.keys())
+print(dict.values())
